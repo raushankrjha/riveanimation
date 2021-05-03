ca4c2eebadb4ef76a004ee7486ab40544aba17ac
raushankrjha <raushanjha465@gmail.com>
2021-05-03 14:17:08 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Animation with rive',
+      home: Scaffold(
+        body: SplashPage(),
+      ),
+    );
+  }
+}
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  final riveFileName = 'assets/tn.riv';
+  Artboard _artboard;
+
+  @override
+  void initState() {
+    _loadRiveFile();
+    super.initState();
+  }
+
+  // loads a Rive file
+  void _loadRiveFile() async {
+    final bytes = await rootBundle.load(riveFileName);
+    final file = RiveFile();
+
+    if (file.import(bytes)) {
+      // Select an animation by its name
+      setState(() => _artboard = file.mainArtboard
+        ..addController(
+          SimpleAnimation('idle'),
+        ));
+    }
+  }
+
+  /// Show the rive file, when loaded
+  @override
+  Widget build(BuildContext context) {
+    return _artboard != null
+        ? Rive(
+            artboard: _artboard,
+            fit: BoxFit.cover,
+          )
+        : Container();
+  }
+}
